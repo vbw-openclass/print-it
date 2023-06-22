6d7f66d77fee368ed27dba076abd044d892beb3c
Vincent Billerey <vincent.billerey.web@gmail.com>
2023-06-22 10:13:00 +0200

    étape 4 et 5 effectuées

--- assets/script.js
+++ assets/script.js
@@ -19,18 +19,60 @@ const slides = [
 
 
 
-// Sélection de l'élément de la flèche gauche
+// Sélection de l'élément de la flèche gauche et droite
 const arrowLeft = document.querySelector('.arrow_left');
-
-// Ajout de l'écouteur d'événement sur le clic
-arrowLeft.addEventListener('click', function() {
-    alert("La flèche gauche a été cliquée !");
-});
-
-// Sélection de l'élément de la flèche droite
 const arrowRight = document.querySelector('.arrow_right');
+const bannerImg = document.querySelector('.banner-img');
+const tagLineElement = document.querySelector('#banner p');
+const dots = document.querySelector('.dot');
+// Variable pour suivre l'index de la diapositive active
+let currentSlideIndex = 0;
+
+
+// Fonction pour mettre à jour la diapositive active
+function updateSlide() {
+  const currentSlide = slides[currentSlideIndex];
+  bannerImg.src = `./assets/images/slideshow/${currentSlide.image}`;
+  tagLineElement.innerHTML = currentSlide.tagLine;
+
+  // Mise à jour des points
+  dots.forEach((dot, index) => {
+    if (index === currentSlideIndex) {
+      dot.classList.add('dot_selected');
+    } else {
+      dot.classList.remove('dot_selected');
+    }
+  });
+}
+
+// Fonction pour passer à la diapositive suivante
+function nextSlide() {
+  if (currentSlideIndex === slides.length - 1) {
+    // Si on est à la dernière diapositive, on revient à la première
+    currentSlideIndex = 0;
+  } else {
+    // Sinon, on passe à la diapositive suivante
+    currentSlideIndex += 1;
+  }
+  updateSlide();
+}
+
+// Fonction pour passer à la diapositive précédente
+function previousSlide() {
+  if (currentSlideIndex === 0) {
+    // Si on est à la première diapositive, on passe à la dernière
+    currentSlideIndex = slides.length - 1;
+  } else {
+    // Sinon, on passe à la diapositive précédente
+    currentSlideIndex -= 1;
+  }
+  updateSlide();
+}
+
 
 // Ajout de l'écouteur d'événement sur le clic
-arrowRight.addEventListener('click', function() {
-    alert("La flèche droite a été cliquée !");
-});
+arrowLeft.addEventListener('click', previousSlide);
+
+
+// Ajout de l'écouteur d'événement sur le clic
+arrowRight.addEventListener('click', nextSlide);
